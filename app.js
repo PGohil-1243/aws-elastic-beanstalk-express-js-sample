1b19fc09d94e0f5c5f9ca5ff518f28f9b68f0a36
PGohil-1243 <priyanshu.gohil@intuitive.cloud>
2023-10-12 18:57:38 +0530

    Full pipeline test

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const port = 8080;
 
-app.get('/', (req, res) => res.send('Hello DevOps World!!!'));
+app.get('/', (req, res) => res.send('Congratulations! You have a fully functional continuous delivery pipeline hosted on AWS.'));
 
 app.listen(port);
 console.log(`App running on http://localhost:${port}`);
